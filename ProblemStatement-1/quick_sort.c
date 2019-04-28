6c10bea95a4cb4a8cffb5e880dcbe5eb41bffe29
PiyushSawarkar <44064862+PiyushSawarkar@users.noreply.github.com>
2019-04-28 10:41:33 +0530

    Update quick_sort.c

--- ProblemStatement-1/quick_sort.c
+++ ProblemStatement-1/quick_sort.c
@@ -1,42 +1,43 @@
 #include<stdio.h>
-void quicksort(int [],int,int);
-void quicksort(int x[10],int first,int last){
-int pivot,i,j,t;
-if(first<last){
-pivot=first;
-i=first;
-j=last;
-while(i<j){
-while(x[i]<=x[pivot]&&i<last)
-i++;
-while(x[j]>x[pivot])
-j--;
-if(i<j){
-t=x[i];
-x[i]=x[j];
-x[j]=t;
+
+void quicksort(int[], int, int);
+void quicksort(int x[10], int first, int last) {
+    int pivot, i, j, t;
+    if (first < last) {
+        pivot = first;
+        i = first;
+        j = last;
+        while (i < j) {
+            while (x[i] <= x[pivot] && i < last)
+                i++;
+            while (x[j] > x[pivot])
+                j--;
+            if (i < j) {
+                t = x[i];
+                x[i] = x[j];
+                x[j] = t;
+            }
+        }
+        t = x[pivot];
+        x[pivot] = x[j];
+        x[j] = t;
+        quicksort(x, first, j - 1);
+        quicksort(x, j + 1, last);
+    }
 }
-}
-t=x[pivot];
-x[pivot]=x[j];
-x[j]=t;
-quicksort(x,first,j-1);
-quicksort(x,j+1,last);
-}
-}
-int main(void){
-int x[10],i,n;
-printf("enter number of elements:\n");
-scanf("%d",&n);
-printf("enter the elements:\n");
-for(i=0;i<n;i++)
-scanf("%d",&x[i]);
-quicksort(x,0,n-1);
-printf("\nsorted elements are:\n");
-for(i=0;i<n;i++)
-printf(" %d",x[i]);
-printf("\n");
-return(0);
+int main(void) {
+    int x[10], i, n;
+    printf("enter number of elements:\n");
+    scanf("%d", & n);
+    printf("enter the elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", & x[i]);
+    quicksort(x, 0, n - 1);
+    printf("\nsorted elements are:\n");
+    for (i = 0; i < n; i++)
+        printf(" %d", x[i]);
+    printf("\n");
+    return (0);
 }
 /* Output of Code:
 apsit@17102021:~$ gcc quick_sort.c -o 17102021
